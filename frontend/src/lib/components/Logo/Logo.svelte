4876a153b1ebb180c554f3f1da1f76ddd56096ba
Dema Saleh <Dema.saleh@diverseltd.com>
2025-05-22 12:35:10 +0300

    Changed logo

--- frontend/src/lib/components/Logo/Logo.svelte
+++ frontend/src/lib/components/Logo/Logo.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import ciso from '$lib/assets/ciso.svg';
+	import ciso from '$lib/assets/diverse.svg';
 
 	export let height = 200;
 	export let width = 200;
 </script>
 
-<img class="c" {height} {width} src={ciso} alt="Ciso-assistant icon" />
+<img class="c" {height} {width} src={ciso} alt="diverse-assistant icon" />
